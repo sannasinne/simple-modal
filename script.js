918aa21554edf645cb571ca2df19da25d12a4795
sannasinne <sanna@keistinen.fi>
2024-04-04 14:56:09 +0300

    Javascript for the modal added

--- script.js
+++ script.js
@@ -7,7 +7,7 @@
 */
 const modal = document.querySelector("#modal");
 const openModalBtn = document.querySelector("#open-modal-btn");
-const closeModalBtn = document.querySelector("close-modal-btn");
+const closeModalBtn = document.querySelector("#close-modal-btn");
 const overlay = document.querySelector("#overlay");
 
 // TODO: 3. Create a click event listener for the open-modal-btn that adds the class "open" to the modal
@@ -19,6 +19,13 @@ openModalBtn.addEventListener("click", (e) => {
 
 // TODO: 4. Create a click event listener for the close-modal-btn that removes the class "open" from the modal
 // BONUS: Also remove the class "open" from the overlay
-
+closeModalBtn.addEventListener("click", (e) => {
+  modal.classList.remove("open");
+  overlay.classList.remove("open");
+});
 
 // BONUS: Add a click event listener to the overlay that removes the class "open" from the modal and the overlay
+overlay.addEventListener("click", (e) => {
+  modal.classList.remove("open");
+  overlay.classList.remove("open");
+});
